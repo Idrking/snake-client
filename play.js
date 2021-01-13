7e59733e124ef6938a6f0ccd208403ce6061b598
Isaac King <idrkings@gmail.com>
2021-01-13 20:53:01 +0000

    Added ability to quit out with CTRL + C

--- play.js
+++ play.js
@@ -1,3 +1,23 @@
 const {connect} = require('./client');
+const setupInput = () => {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+
+  const handleUserInput = function(key) {
+    if (key === '\u0003') {
+      process.exit();
+    }
+  };
+
+  stdin.on('data', (key) => {
+    handleUserInput(key);
+  });
+  return stdin;
+};
+
+
 console.log('Connecting...');
 connect();
+setupInput();
