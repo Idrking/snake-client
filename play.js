136d53d75241e88e6b6123059f5568d6463f9d63
Isaac King <idrkings@gmail.com>
2021-01-13 20:30:53 +0000

    Moved client establishment to its own function

--- play.js
+++ play.js
@@ -1,20 +1,4 @@
-const net = require('net');
-
-const connect = () => {
-  const conn = net.createConnection({
-    host: '135.23.222.131',
-    port: 50542
-  });
-
-  conn.setEncoding('utf8');
-  
-  conn.on('data', (data) => {
-    console.log(data);
-  })
-
-  return conn;
-
-}
-
+const {connect} = require('./client');
+console.log(connect);
 console.log('Connecting...');
 connect();
