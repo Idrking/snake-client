a3a4cd30d54d688f00dd3abd37e33b84454fc011
Isaac King <idrkings@gmail.com>
2021-01-13 22:14:01 +0000

    Moved input setup to it's own file

--- play.js
+++ play.js
@@ -1,21 +1,5 @@
 const {connect} = require('./client');
-const setupInput = () => {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-
-  const handleUserInput = function(key) {
-    if (key === '\u0003') {
-      process.exit();
-    }
-  };
-
-  stdin.on('data', (key) => {
-    handleUserInput(key);
-  });
-  return stdin;
-};
+const {setupInput} = require('./input.js');
 
 
 console.log('Connecting...');
